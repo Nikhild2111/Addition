aff824864ab106d26cafb87fed7ff4443730f009
sdabhade <sahildabhade21@gmail.com>
2023-06-14 16:39:42 +0530

    commit and push Addition.java

---
+++ addition/src/Addition.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Addition
+{
+    public static void main(String[] args)
+    {
+        int a,b,c;
+        a=8;
+        b=2;
+        c=a*b;
+
+        System.out.println(c);
+
+    }
+
+
+}
